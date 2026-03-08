a40226c0be990d13ba249088e37e1d3ad445c804
HobbyRabbit <stephencewood@gmail.com>
2026-03-07 21:48:01 -0500

    Update sensor.py

--- custom_components/ac_infinity/sensor.py
+++ custom_components/ac_infinity/sensor.py
@@ -1,145 +1,37 @@
 """The ac_infinity sensor platform."""
 from __future__ import annotations
-from typing import Any
 
-from ac_infinity_ble import ACInfinityController
+from homeassistant.components.sensor import SensorEntity
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
-from homeassistant.components.sensor import (
-    SensorDeviceClass,
-    SensorEntity,
-    SensorStateClass,
-)
-
-from homeassistant.components.bluetooth.passive_update_coordinator import (
-    PassiveBluetoothCoordinatorEntity,
-)
-from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import PERCENTAGE, UnitOfPressure, UnitOfTemperature
-from homeassistant.core import HomeAssistant, callback
-from homeassistant.helpers import device_registry as dr
-from homeassistant.helpers.entity import DeviceInfo
-from homeassistant.helpers.entity_platform import AddEntitiesCallback
-
-from .const import DEVICE_MODEL, DOMAIN
-from .coordinator import ACInfinityDataUpdateCoordinator
-from .models import ACInfinityData
+from .coordinator import ACInfinityCoordinator
+from . import DOMAIN
 
 
-async def async_setup_entry(
-    hass: HomeAssistant,
-    entry: ConfigEntry,
-    async_add_entities: AddEntitiesCallback,
-) -> None:
-    """Set up the light platform for LEDBLE."""
-    data: ACInfinityData = hass.data[DOMAIN][entry.entry_id]
+async def async_setup_entry(hass, entry, async_add_entities):
+    coordinator: ACInfinityCoordinator = hass.data[DOMAIN][entry.entry_id]
+
     entities = [
-        TemperatureSensor(data.coordinator, data.device, entry.title),
-        HumiditySensor(data.coordinator, data.device, entry.title),
+        ACInfinityTemperatureSensor(coordinator),
+        ACInfinityHumiditySensor(coordinator),
     ]
-    if data.device.state.version >= 3 and data.device.state.type in [7, 9, 11, 12]:
-        entities.append(VpdSensor(data.coordinator, data.device, entry.title))
+
     async_add_entities(entities)
 
 
-class ACInfinitySensor(
-    PassiveBluetoothCoordinatorEntity[ACInfinityDataUpdateCoordinator], SensorEntity
-):
-    """Representation of AC Infinity sensor."""
-
-    def __init__(
-        self,
-        coordinator: ACInfinityDataUpdateCoordinator,
-        device: ACInfinityController,
-        name: str,
-    ) -> None:
-        """Initialize an AC Infinity sensor."""
-        super().__init__(coordinator)
-        self._device = device
-        self._name = name
-        self._attr_device_info = DeviceInfo(
-            name=device.name,
-            model=DEVICE_MODEL[device.state.type],
-            manufacturer="AC Infinity",
-            sw_version=device.state.version,
-            connections={(dr.CONNECTION_BLUETOOTH, device.address)},
-        )
-        self._async_update_attrs()
-
-    @callback
-    def _async_update_attrs(self) -> None:
-        """Handle updating _attr values."""
-        raise NotImplementedError("Not yet implemented.")
-
-    @callback
-    def _handle_coordinator_update(self, *args: Any) -> None:
-        """Handle data update."""
-        self._async_update_attrs()
-        self.async_write_ha_state()
-
-    async def async_added_to_hass(self) -> None:
-        """Register callbacks."""
-        self.async_on_remove(
-            self._device.register_callback(self._handle_coordinator_update)
-        )
-        return await super().async_added_to_hass()
-
-
-class TemperatureSensor(ACInfinitySensor):
-    _attr_native_unit_of_measurement = UnitOfTemperature.CELSIUS
-    _attr_device_class = SensorDeviceClass.TEMPERATURE
-    _attr_state_class = SensorStateClass.MEASUREMENT
+class ACInfinityTemperatureSensor(CoordinatorEntity, SensorEntity):
+    _attr_name = "AC Infinity Temperature"
+    _attr_native_unit_of_measurement = "°C"
 
     @property
-    def name(self) -> str:
-        return f"{self._name} Temperature"
+    def native_value(self):
+        return self.coordinator.data.get("temperature")
+
+
+class ACInfinityHumiditySensor(CoordinatorEntity, SensorEntity):
+    _attr_name = "AC Infinity Humidity"
+    _attr_native_unit_of_measurement = "%"
 
     @property
-    def unique_id(self) -> str:
-        """Return a unique, Home Assistant friendly identifier for this entity."""
-        return f"{self._device.address}_tmp"
-
-    @callback
-    def _async_update_attrs(self) -> None:
-        """Handle updating _attr values."""
-        self._attr_native_value = self._device.temperature
-
-
-class HumiditySensor(ACInfinitySensor):
-    _attr_name = "Humidity"
-    _attr_native_unit_of_measurement = PERCENTAGE
-    _attr_device_class = SensorDeviceClass.HUMIDITY
-    _attr_state_class = SensorStateClass.MEASUREMENT
-
-    @property
-    def name(self) -> str:
-        return f"{self._name} Humidity"
-
-    @property
-    def unique_id(self) -> str:
-        """Return a unique, Home Assistant friendly identifier for this entity."""
-        return f"{self._device.address}_hum"
-
-    @callback
-    def _async_update_attrs(self) -> None:
-        """Handle updating _attr values."""
-        self._attr_native_value = self._device.humidity
-
-
-class VpdSensor(ACInfinitySensor):
-    _attr_native_unit_of_measurement = UnitOfPressure.KPA
-    _attr_device_class = SensorDeviceClass.ATMOSPHERIC_PRESSURE
-    _attr_state_class = SensorStateClass.MEASUREMENT
-
-    @property
-    def name(self) -> str:
-        return f"{self._name} VPD"
-
-    @property
-    def unique_id(self) -> str:
-        """Return a unique, Home Assistant friendly identifier for this entity."""
-        return f"{self._device.address}_vpd"
-
-    @callback
-    def _async_update_attrs(self) -> None:
-        """Handle updating _attr values."""
-        self._attr_native_value = self._device.vpd
+    def native_value(self):
+        return self.coordinator.data.get("humidity")
