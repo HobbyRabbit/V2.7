2f39a7343dcca039347a66ee88573fe8a484281c
HobbyRabbit <stephencewood@gmail.com>
2026-03-13 22:12:04 -0400

    Update __init__.py

--- custom_components/ac_infinity/__init__.py
+++ custom_components/ac_infinity/__init__.py
@@ -1,24 +1,26 @@
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
+from .const import DOMAIN
 from .coordinator import ACInfinityCoordinator
-
-DOMAIN = "ac_infinity"
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
-    address = entry.data["address"]
-    name = entry.title
 
-    coordinator = ACInfinityCoordinator(hass, address, name)
+    coordinator = ACInfinityCoordinator(
+        hass,
+        entry.data["mac"],
+        entry.title,
+    )
 
     await coordinator.async_config_entry_first_refresh()
 
-    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = coordinator
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][entry.entry_id] = coordinator
 
     await hass.config_entries.async_forward_entry_setups(
         entry,
-        ["switch", "fan", "sensor"],
+        ["fan","switch","sensor"]
     )
 
     return True
